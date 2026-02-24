58008daecf7556b108d7421280b73ced1a05ae17
arc <zaveyborodaid@gmail.com>
2026-02-24 09:14:11 +0500

    done

---
+++ chap4/or-channel/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	orChannel()
+}
+
+func orChannel() {
+	sig := func(after time.Duration) <-chan any {
+		c := make(chan any)
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+	fmt.Printf("done after %v", time.Since(start))
+}
+
+func or(channels ...<-chan any) <-chan any {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan any)
+	go func() {
+		defer close(orDone)
+
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-or(append(channels[3:], orDone)...): // pass orDone to terminate child or-channels when curr is done
+			}
+		}
+	}()
+
+	return orDone
+}
